860a7abb436950a68e4dc9b2700bbd0cfbdce070
Shubham Banerjee <shubham.banerjee@comviva.com>
2020-03-05 12:21:55 +0530

    automated json req addition

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react'
 import {robots} from './robots'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
+import './App.css'
 
 class App extends Component{
 
     constructor(props) {
         super(props)
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))
     }
 
     onSearchChange = (event) => {
@@ -24,10 +31,14 @@ class App extends Component{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         }) 
 
+        if(this.state.robots.length === 0){
+            return <h1 className='tc'>Loading...</h1>
+        }
+
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <h1 className='grow shadow-100 f1'>Robots</h1><hr/>
+                <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots}/>
             </div>
         )
